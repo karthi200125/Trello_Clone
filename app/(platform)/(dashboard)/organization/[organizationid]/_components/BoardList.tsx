8f48a9f7771f7f481861199a1d59550a88c055dc
karthi200125 <skarthikeyan25052001@gmail.com>
2024-01-10 14:28:30 +0530

    get boards and display

--- app/(platform)/(dashboard)/organization/[organizationid]/_components/BoardList.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/_components/BoardList.tsx
@@ -2,9 +2,27 @@ import { FormPopover } from "@/components/form/FromPopover"
 import { Hint } from "@/components/hint"
 
 import { HelpCircle, User2 } from "lucide-react"
+import { auth } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import Link from "next/link";
+
+export const BoardList = async () => {
+
+    const { orgId } = auth();
+
+    if (!orgId) return redirect("/selectorg")
+
+    const Boards = await db.board.findMany({
+        where: {
+            orgId,
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
 
 
-export const BoardList = () => {
     return (
         <div className="space-y-4">
             <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -12,6 +30,13 @@ export const BoardList = () => {
                 Your Boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                {Boards.map((board) => (
+                    <Link href={`/board/${board.id}`} key={board.id} style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+                        className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden">
+                        <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+                        <p className="relative font-semibold text-white">{board.title}</p>
+                    </Link>
+                ))}
                 <FormPopover sideOffset={10} side="right">
                     <div role="button" className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
                         <p className="text-sm">Create new board</p>
