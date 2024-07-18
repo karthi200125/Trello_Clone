cc304c9849b1010565ca9cfb7d7b614144092c9c
Kathick <skarthikeyan25052001@gmail.com>
2024-07-18 11:00:00 +0530

    complete descript on update

--- actions/UpdateCard/Schema.ts
+++ actions/UpdateCard/Schema.ts
@@ -7,11 +7,11 @@ export const UpdateCard = z.object({
             required_error: "Description id required",
             invalid_type_error: "descript is required",
         }).min(3, { message: "description is too short" })),
-    title: z.string({
+    title: z.optional(z.string({
         required_error: "Tilte is Required",
         invalid_type_error: "Tilte is Required"
     }).min(3, {
         message: "Title is too short"
-    }),
+    })),
     id: z.string()
 })
