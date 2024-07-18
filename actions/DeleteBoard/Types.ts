4fee893b53e0179046e3a7ac84f01aaac48c00b5
Kathick <skarthikeyan25052001@gmail.com>
2024-07-18 18:06:22 +0530

    chnag board to Board

--- actions/DeleteBoard/Types.ts
+++ actions/DeleteBoard/Types.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 import { ActionState } from '@/lib/createsafeaction';
-import { board } from '@prisma/client';
+import { Board } from '@prisma/client';
 import { DeleteBoard } from './Schema';
 
 
 export type InputType = z.infer<typeof DeleteBoard>;
-export type ReturnType = ActionState<InputType, board>
+export type ReturnType = ActionState<InputType, Board>
