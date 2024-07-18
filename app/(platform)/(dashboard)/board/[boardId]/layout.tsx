9f888bd0de4e430e8e32a7f6176f6437ec09b790
Kathick <skarthikeyan25052001@gmail.com>
2024-07-18 17:59:40 +0530

    again

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -3,25 +3,6 @@ import { auth } from "@clerk/nextjs";
 import { notFound, redirect } from "next/navigation";
 import { BoardNavbar } from "./_components/BoardNavbar";
 
-// export async function generateMetaData({ params }: { params: { boardId: string } }) {
-//     const { orgId } = auth()
-
-//     if (!orgId) return { title: "Board" }
-
-//     const board = await db.board.findUnique({
-//         where: {
-//             id: params.boardId,
-//             orgId
-//         }
-//     })
-
-//     return { title: board?.title || "Baord" }
-
-// }
-
-
-
-
 const BoardIdLayout = async ({ children, params }: { children: React.ReactNode; params: { boardId: string } }) => {
 
     const { orgId } = auth()
